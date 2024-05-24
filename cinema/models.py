90d5cc4083ec357ff5014b2e8061c19397bf8eb8
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 08:01:39 +0300

    Create model Genre

--- cinema/models.py
+++ cinema/models.py
@@ -9,6 +9,13 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
