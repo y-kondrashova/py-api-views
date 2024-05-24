c7d6c4e779afc72355c345931491e24cd06932bc
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 15:24:54 +0300

    Add path to cinema-hall-list in urls.py cinema app

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,17 +1,23 @@
 from django.urls import path
 
 from cinema.views import (
-    ActorList,
     ActorDetail,
+    ActorList,
+    CinemaHallList,
     GenreDetail,
     GenreList,
-    movie_list,
-    movie_detail
+    movie_detail,
+    movie_list
 )
 
 urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path(
+        "cinema-halls/",
+        CinemaHallList.as_view(actions={"get": "list", "post": "create"}),
+        name="cinema-hall-list"
+    ),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("movies/", movie_list, name="movie-list"),
