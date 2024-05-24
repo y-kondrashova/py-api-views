fa9612a38b5324c90711b095bbcf2fda46856ca4
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 16:59:40 +0300

    Refactor urls.py
    Fix flake8 error:
     -  E501 line too long (82 > 79 characters)

--- cinema/urls.py
+++ cinema/urls.py
@@ -33,7 +33,11 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema-halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
+    ),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
 ]
