424713e06ea52c78375a967b1623085abc0664f9
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 08:40:35 +0300

    Create ActorSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,4 +1,25 @@
 from rest_framework import serializers
+from cinema.models import Actor, CinemaHall, Movie, Genre
+
+
+class ActorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name
+        )
+        instance.save()
+        return instance
+
 
 from cinema.models import Movie
 
