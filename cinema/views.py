a7dc2ff9083d5f516016e995ca43b201009039fe
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 13:58:04 +0300

    Create GenreList view with APIView

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,15 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 
 from django.shortcuts import get_object_or_404
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
+from cinema.models import Movie, Genre
+from cinema.serializers import MovieSerializer, GenreSerializer
 
 
 @api_view(["GET", "POST"])
@@ -43,3 +47,17 @@ def movie_detail(request, pk):
     if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class GenreList(APIView):
+    def get(self, request) -> Response:
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request) -> Response:
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
