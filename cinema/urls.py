1dc76ae313531b34dd9280eb146e4c0e8154e7a7
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 14:51:18 +0300

    Add path to actor-detail in urls.py cinema app

--- cinema/urls.py
+++ cinema/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from cinema.views import (
     ActorList,
+    ActorDetail,
     GenreDetail,
     GenreList,
     movie_list,
@@ -10,6 +11,7 @@ from cinema.views import (
 
 urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("movies/", movie_list, name="movie-list"),
