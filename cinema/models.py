89b29e6607485cb58d4b1872778612e0b715da91
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 08:01:02 +0300

    Create model Actor

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
