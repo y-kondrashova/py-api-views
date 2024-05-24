7791dbef31f16fb9bc88a21045073ade0eaa1c6b
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 14:02:36 +0300

    Add path to genre-list in urls.py cinema app

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    GenreDetail,
+    GenreList,
+    movie_list,
+    movie_detail
+)
 
 urlpatterns = [
+    path("genres/", GenreList.as_view(), name="genre-list"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
