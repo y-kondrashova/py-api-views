dd19a78ddf6dfbbe5eed0b2c30e8b2d78cb97222
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 15:23:33 +0300

    Create CinemaHallList view with GenericViewSet

--- cinema/views.py
+++ cinema/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status, generics, mixins
+from rest_framework import generics, mixins, status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre, Actor
+from cinema.models import Actor, CinemaHall, Genre, Movie
 from cinema.serializers import (
-    GenreSerializer,
     ActorSerializer,
-    MovieSerializer,
+    CinemaHallSerializer,
+    GenreSerializer,
+    MovieSerializer
 )
 
 
@@ -39,6 +40,15 @@ class ActorDetail(
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class CinemaHallList(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
 
 
 @api_view(["GET", "POST"])
