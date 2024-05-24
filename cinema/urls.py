f9dc73bda5cdfbb140640c6142e9da11e49ac10c
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 17:58:05 +0300

    Fix urls.py with spelling problem

--- cinema/urls.py
+++ cinema/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("", include(router.urls)),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema-halls/<int:pk>/",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     ),
