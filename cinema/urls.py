177b8229d9dd64bce9e1c9b68fcb31c5bf7347da
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-24 16:41:36 +0300

    Refactor urls.py
    Add variables:
     - cinema_hall_list
     - cinema_hall_detail

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,34 +3,33 @@ from django.urls import path
 from cinema.views import (
     ActorDetail,
     ActorList,
-    CinemaHallDetail,
-    CinemaHallList,
+    CinemaHallViewSet,
     GenreDetail,
     GenreList,
     movie_detail,
     movie_list
 )
 
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create"
+    }
+)
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
+
 urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path(
-        "cinema-halls/",
-        CinemaHallList.as_view(actions={"get": "list", "post": "create"}),
-        name="cinema-hall-list"
-    ),
-    path(
-        "cinema-halls/<int:pk>/",
-        CinemaHallDetail.as_view(
-            actions={
-                "get": "retrieve",
-                "put": "update",
-                "patch": "partial_update",
-                "delete": "destroy"
-            }
-        ),
-        name="cinema-hall-detail"
-    ),
+    path("cinema-halls/", cinema_hall_list, name="cinema-hall-list"),
+    path("cinema-halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("movies/", movie_list, name="movie-list"),
